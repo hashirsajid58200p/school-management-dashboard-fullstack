51873a5488fe6046af6a06422efa20c1997138a7
hashirsajid58200p <hs58200p@gmail.com>
2026-07-18 15:41:32 +0500

    style: fix layout width constraints for Parent dashboard schedules

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -17,15 +17,13 @@ const ParentPage = async () => {
   return (
     <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
       {/* LEFT */}
-      <div className="">
+      <div className="w-full xl:w-2/3 flex flex-col gap-8">
         {students.map((student) => (
-          <div className="w-full xl:w-2/3" key={student.id}>
-            <div className="h-full bg-white p-4 rounded-md">
-              <h1 className="text-xl font-semibold">
-                Schedule ({student.name + " " + student.surname})
-              </h1>
-              <BigCalendarContainer type="classId" id={student.classId} />
-            </div>
+          <div className="bg-white p-4 rounded-md" key={student.id}>
+            <h1 className="text-xl font-semibold mb-4">
+              Schedule ({student.name + " " + student.surname})
+            </h1>
+            <BigCalendarContainer type="classId" id={student.classId} />
           </div>
         ))}
       </div>
